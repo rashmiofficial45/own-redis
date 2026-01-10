7455b7f4f7c60664ff4e488c2426bd9ee37d2391
Rashmi Ranjan Parija <126317142+rashmiofficial45@users.noreply.github.com>
2026-01-10 16:36:46 +0530

    added functionalities for get in redis

--- index.js
+++ index.js
@@ -5,29 +5,38 @@ import net from "net";
 import Parser from "redis-parser";
 // Redis default port is 6379
 const port = 6379;
-const store = {}
+const store = {};
 // Create a TCP server that handles incoming connections
 const server = net.createServer((connection) => {
   // Handle incoming data from the client
   connection.on("data", (data) => {
     const parser = new Parser({
       returnReply: (reply) => {
-        const command = reply[0]
-        switch(command){
-          case 'set': {
-            const key = reply[1]
-            const value = reply[2]
-            store[key] = value
-            connection.write("+Ok\r\n")
-          }
-          break
+        const command = reply[0];
+        switch (command) {
+          case "set":
+            {
+              const key = reply[1];
+              const value = reply[2];
+              store[key] = value;
+              connection.write("+Ok\r\n");
+            }
+            break;
+          case "get":
+            {
+              const key = reply[1];
+              const value = store[key];
+              if (!value) connection.write("$-1\r\n");
+              connection.write(`+${value}\r\n`);
+            }
+            break;
         }
       },
       returnError: (err) => {
-        console.log("error => ",err)
-      }
+        console.log("error => ", err);
+      },
     });
-    parser.execute(data)
+    parser.execute(data);
 
     // Respond with Redis protocol simple string reply (+OK\r\n)
   });
